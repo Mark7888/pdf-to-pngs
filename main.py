ae2ce2bbc012391282811f7039e4cd47c7839c09
Mark7888 <l.mark7888@gmail.com>
2024-02-05 14:35:01 +0100

    error handling in the main process

--- main.py
+++ main.py
@@ -23,19 +23,19 @@ def convert_pdf_to_png(pdf_path, output_directory):
     doc.close()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()
-
-    file_path = filedialog.askopenfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")], title="Choose a file to open")
-
-    save_dir = filedialog.askdirectory(initialdir=file_path.rsplit("/", 1)[0], title="Choose a directory to save")
-
-    # make new directory
-    new_dir = save_dir + "/output_" + file_path.rsplit("/", 1)[1]
-    if not os.path.exists(new_dir):
-        os.mkdir(new_dir)
-
     try:
+        root = tk.Tk()
+        root.withdraw()
+
+        file_path = filedialog.askopenfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")], title="Choose a file to open")
+
+        save_dir = filedialog.askdirectory(initialdir=file_path.rsplit("/", 1)[0], title="Choose a directory to save")
+
+        # make new directory
+        new_dir = save_dir + "/output_" + file_path.rsplit("/", 1)[1]
+        if not os.path.exists(new_dir):
+            os.mkdir(new_dir)
+    
         convert_pdf_to_png(file_path, new_dir)
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred: {e}")
